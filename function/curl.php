c1c7919770508337bfc467ebb8a7166a13d35b20
humeng93 <354678192@qq.com>
2016-03-13 22:30:59 +0800

    Feature: 添加新功能

--- function/curl.php
+++ function/curl.php
@@ -1,6 +1,48 @@
 <?php
 
 /**
+ * 模拟GET请求
+ */
+function curl_get($url, $data = array(), $header = array()){
+
+    $cu = curl_init();
+    curl_setopt($cu, CURLOPT_URL, $url);
+    curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
+    $ret = curl_exec($cu);
+    curl_close($cu);
+    return $ret;
+}
+
+/**
+ * 模拟post请求
+ * @param $url
+ * @param $data
+ * @return error or 数据
+ */
+function curl_post($url, $data = array(), $header = array()){   
+    $curl = curl_init(); // 启动一个CURL会话      
+    curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址                  
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查      
+    // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2); // 从证书中检查SSL加密算法是否存在      
+    curl_setopt($curl, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); // 模拟用户使用的浏览器      
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转      
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer      
+    curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求      
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data); // Post提交的数据包      
+    // curl_setopt($curl, CURLOPT_COOKIEFILE, $GLOBALS['cookie_file']); // 读取上面所储存的Cookie信息      
+    curl_setopt($curl, CURLOPT_TIMEOUT, 60); // 设置超时限制防止死循环
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $header);      
+    curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容      
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回      
+    $tmpInfo = json_decode(curl_exec($curl), true); // 执行操作      
+    if (curl_errno($curl)) {      
+       echo 'Errno'.curl_error($curl);      
+    }      
+    curl_close($curl); // 关键CURL会话      
+    return $tmpInfo; // 返回数据      
+}
+
+/**
  * 以post方式提交xml到对应的接口url
  * 
  * @param string $xml  需要post的xml数据
